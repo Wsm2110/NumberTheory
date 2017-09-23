6921a6eb477ad05d7571a7cce1d2cec263a0def3
Dev01\Wiljan <wruizendaal@gmail.com>
2017-09-23 14:09:08 +0200

    multiplication table(3) without actually multiplying. Same speed as multiplying, but generates 3 times more output

---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = 3;
+            var b = 6;
+            var c = 9;
+                        
+            var s1 = Stopwatch.StartNew();
+
+            for (int i = 1; i < 3000; i++)
+            {
+
+                if (a == 0)
+                {
+                    a = 9;
+                }
+
+                if (b == 0)
+                {
+                    b = 9;
+                }
+
+                if (c == 0)
+                {
+                    c = 9;
+                }
+
+                a = --a;
+                b = --b;
+                c = --c;
+
+                Trace.WriteLine($"{i}{a} {i}{b} {i}{c} { (a == 0 ? $"{i}9" : "")}");
+
+                if (a == 0)
+                {
+                    a = b;
+                    b = c;
+                    c = 9;
+                }
+
+            }
+
+            s1.Stop();        
+
+        }
+    }
+}
